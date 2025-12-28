28dbbbf12523b5b39538bd23cbb1d69973237968
Mihir Prajapt <prajpatmihir03@gmail.com>
2025-12-28 18:32:49 +0530

    gtag added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -41,87 +41,9 @@ Version      : 1.0
 					$('.site-navigation, .header-white, .header').removeClass('navbar-fixed');
 				}
 			});		  	
-		/*END MENU JS*/
-		
-		/*START VIDEO JS*/
-		$('.video-play').magnificPopup({
-            type: 'iframe'
-        });
-		$('.co-video-play').magnificPopup({
-            type: 'iframe'
-        });
-		$('.product_item').mixItUp();
-		/*END VIDEO JS*/	
-
-		/* START VIDEO POPUP JS */
-		$('.magnific_popup').magnificPopup({
-		  disableOn: 700,
-		  type: 'iframe',
-		  mainClass: 'mfp-fade',
-		  removalDelay: 160,
-		  preloader: false,
-		  fixedContentPos: false,
-		  disableOn: 300
-		});			
-
-		/*START PARTNER LOGO*/
-		$('.partner').owlCarousel({
-		  autoPlay: 3000, //Set AutoPlay to 3 seconds
-		  items : 5,
-		  itemsDesktop : [1199,3],
-		  itemsDesktopSmall : [979,3]
-		});
-		/*END PARTNER LOGO*/		
-
-		/*START TESTIMONIAL JS*/	
-		$("#testimonial-slider").owlCarousel({
-		   items:3,
-			itemsDesktop:[1000,3],
-			itemsDesktopSmall:[980,2],
-			itemsTablet:[768,2],
-			itemsMobile:[650,1],
-			pagination:true,
-			navigation:true,
-			navigationText:["",""],
-			slideSpeed:1000,
-			autoPlay:false
-		});
-		/*END TESTIMONIAL JS*/
-
-		/* START EVENT JS */
-		 $("#event-slider").owlCarousel({
-			items:3,
-			itemsDesktop:[1199,3],
-			itemsDesktopSmall:[979,2],
-			itemsTablet:[768,2],
-			itemsMobile:[600,1],
-			pagination: false,
-			navigation:true,
-			navigationText:["",""],
-			slideSpeed:1000,
-			autoPlay:false
-		});
-		/* END EVENT JS */		
+		/*END MENU JS*/						
 			
 	}); 		
-	
-	/*START RANGE SLIDER*/
-	    var rangeSlider = function(){
-        var slider = $('.range-slider'),
-            range = $('.range-slider input[type="range"]'),
-            value = $('.range-value');
-        slider.each(function(){
-            value.each(function(){
-                var value = $(this).prev().attr('value');
-                $(this).html(value);
-            });
-            range.on('input', function(){
-                $(this).next(value).html(this.value);
-            });
-        });
-    };
-    rangeSlider();
-	/*END RANGE SLIDER*/
 	
 	/*INITIATE PURE COUNTER*/
 		new PureCounter();
